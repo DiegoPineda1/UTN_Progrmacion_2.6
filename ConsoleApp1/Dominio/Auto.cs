2991c830a313d768ccae4c2cbf4df1115ed1e673
Diego Pineda <412672@tecnicatura.frc.utn.edu.ar>
2024-04-11 13:59:58 -0300

    initial commit

--- ConsoleApp1/Dominio/Auto.cs
+++ ConsoleApp1/Dominio/Auto.cs
@@ -8,6 +8,34 @@ namespace ConsoleApp1.Dominio
 {
     internal class Auto
     {
+        public double cKilometros {  get; set; }
+        public double comustible {  get; set; }
+        public int capacidad { get; set; }
+        public double combustibleACargar {  get; set; }
+        public Auto()
+        {
+            this.capacidad = 54;
+        }
+          
+        public bool Conducir()
+        {
+            bool prodra = false;
+            if (comustible * 11 >= cKilometros ) 
+            {
+                prodra = true;
+            }
+            
+            return prodra;
+        }
 
+        public double CargarCombustible()
+        {
+            double perdio = 0;
+            if (combustibleACargar + comustible > capacidad)
+            {
+                perdio = (combustibleACargar + comustible) - capacidad;
+            }
+            return perdio;
+        }
     }
 }
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ConsoleApp1.Dominio;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+       static void Main()
+        {
+            Auto a1 = new Auto();
+
+            Console.WriteLine("Cuanto KM quiere recorrer");
+            a1.cKilometros = double.Parse(Console.ReadLine());
+            Console.WriteLine("Cuanto Combustible tiene");
+            a1.comustible = double.Parse(Console.ReadLine());
+            Console.WriteLine("Usted " + a1.Conducir() + " podra recorer " + a1.cKilometros + "KM");
+
+            Console.WriteLine("Cuanto litros quiere cargar?");
+            a1.combustibleACargar = double.Parse(Console.ReadLine());
+            Console.WriteLine("Usted tuvo una perdida de " + a1.CargarCombustible() + "L");
+        }
+    }
+}
